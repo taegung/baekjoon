7c17e6f16f7b867b75f2b4bd630be02c5c09cc6d
taegung <sungminyun12@daum.net>
2023-12-31 14:19:10 +0900

    팰린드롬수

---
+++ baekjoon1259.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class baekjoon1259 {
+    public static void main(String[] args) {
+        
+      Scanner in=new Scanner(System.in);
+      
+      for(;;){
+          int score = in.nextInt();
+        if(score==0){
+            break;
+        }
+        else{
+            String s=String.valueOf(score);
+            StringBuffer s1=new StringBuffer(s);
+            String reverse =s1.reverse().toString();
+           
+            if(s.equals(reverse)){
+                System.out.println("yes");
+            }
+            else{
+                System.out.println("no");
+            }
+        }
+
+      }
+    }
+}
