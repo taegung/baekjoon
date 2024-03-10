11b2e9cf45a11582c05396fd87ebc392f4c06dd6
taegung <sungminyun12@daum.net>
2024-03-10 18:38:20 +0900

    주식

--- back11501.java
+++ back11501.java
@@ -1,38 +1,42 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
 public class back11501 {
     
-    public static void main(String[] args) {
-        Scanner in =new Scanner(System.in);
+  public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
-        int N= in.nextInt();
-        int arr[]=new int[N];
-        for(int i=0;i<N;i++){
-          arr[i]=in.nextInt();
-        }
-        int pt=arr[0];
-        int sum=0;
-        for(int i=1;i<N;i++){
-           
-            if(pt<arr[i]){
-                sum=pt;
-                pt=arr[i];
-            }
-            else{
-                pt=arr[i];
+        int K = in.nextInt(); 
+        long arr[]=new long[K];
+        for (int q = 0; q < K; q++) {
+            int N = in.nextInt(); 
+            long result = 0;
+            ArrayList<Long> list1 = new ArrayList<>(N);
+            for (int i = 0; i < N; i++) {
+                long num = in.nextLong();
+                list1.add(num);
             }
 
-
-            
+            long max = list1.get(N - 1); 
+            for (int i = N - 2; i >= 0; i--) {
+                if (list1.get(i) < max) {
+                  
+                    result += max - list1.get(i);
+                } else {
+                    
+                    max = list1.get(i);
+                }
+            }
+            arr[q]=result;
         }
 
-
-    
-    
-    
-    
-    
-    
-    
-    
+        for (int i=0; i<K;i++) {
+            System.out.println(arr[i]);
+        }
+        in.close();
     }
+    
+    
+   
+
 }
