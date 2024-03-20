fcb288befa59e5107eefbdff62d901b6c5318ce3
taegung <sungminyun12@daum.net>
2024-03-20 23:13:26 +0900

    프로그래머스(숫자 문자열과 영단어)

--- back2346.java
+++ back2346.java
@@ -1,10 +1,54 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Scanner;
+
 public class back2346 {
     
     public static void main(String[] args) {
         
-        Scanner in =new Scanner(System.in);
-        int N =in.nextInt();
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        Deque<Integer> balloons = new ArrayDeque<>();
+        Deque<Integer> balloonIndexes = new ArrayDeque<>();
+       
+        for (int i = 0; i < N; i++) {
+            int number = in.nextInt();
+            balloons.add(number);
+            balloonIndexes.add(i + 1);
+        }
+
+        while (!balloons.isEmpty()) {
+            int num = balloons.pollFirst();
+            int index = balloonIndexes.pollFirst();
+            System.out.print(index + " ");
+            
+         
+            int move = num;
+            if (move > 0) {
+                move--;
+            }
+
         
+            while (move > 0) {
+                balloons.addLast(balloons.pollFirst());
+                balloonIndexes.addLast(balloonIndexes.pollFirst());
+                move--;
+            }
+
+         
+            while (move < 0) {
+                balloons.addFirst(balloons.pollLast());
+                balloonIndexes.addFirst(balloonIndexes.pollLast());
+                move++;
+            }
+            
+           
+            if (balloons.size() == 1) {
+                System.out.print(balloonIndexes.pollFirst());
+                break;
+            }
+        }
+        
+        in.close();
     }
 }
---
+++ ct1.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class ct1 {
+
+    public static void main(String[] args) {
+        
+        Scanner in =new Scanner(System.in);
+        
+        String s= in.nextLine();
+        HashMap<String,String> h1=new HashMap<String,String>();
+        HashMap<String,String> h2=new HashMap<String,String>();
+
+        
+            h1.put("zero","0");
+            h1.put("one","1");
+            h1.put("two","2");
+            h1.put("three","3");
+            h1.put("four","4");
+            h1.put("five","5");
+            h1.put("six","6");
+            h1.put("seven","7");
+            h1.put("eight","8");
+            h1.put("nine","9");
+            h2.put("0","0");
+            h2.put("1","1");
+            h2.put("2","2");
+            h2.put("3","3");
+            h2.put("4","4");
+            h2.put("5","5");
+            h2.put("6","6");
+            h2.put("7","7");
+            h2.put("8","8");
+            h2.put("9","9");
+            char[] chr = s.toCharArray();
+            String change = "";
+            String result="";
+            for (int i = 0; i < chr.length; i++) {
+                String s1 =Character.toString(chr[i]);
+                if(h2.containsKey(s1)){
+                     result+=s1;
+                }else{
+                    change += s1; 
+                    if(h1.containsKey(change)){
+                        result+=h1.get(change);
+                        change="";
+                    }
+                }
+            }
+            int answer=Integer.parseInt(result);
+
+            System.out.println(answer);
+       
+
+
+
+    }
+    
+}
+
+
